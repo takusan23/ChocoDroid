e7012adc31559e3831f3b9aae56f7e074a5ad86b
takusan23 <takumu1200@gmail.com>
2023-01-02 04:00:18 +0900

    pluginも移行

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-buildscript {
-    val kotlinVersion: String by extra("1.7.20")
-    val composeVersion: String by extra("1.3.1")
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application").version("7.3.0").apply(false)
-    id("com.android.library").version("7.3.0").apply(false)
-    id("org.jetbrains.kotlin.android").version("1.7.20").apply(false)
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
 }
 
 tasks.register("clean") {
